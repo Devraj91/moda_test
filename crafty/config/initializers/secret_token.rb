02c04a6ecca0ee92305c44725898755ee29ee574
Devraj Roy <devraj.roy91@gmail.com>
2017-04-25 23:34:58 +0530

    initialize repo

---
+++ crafty/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crafty::Application.config.secret_key_base = '4b1a801914ebdda8d6f299a42df22785961fb66c226f9f48037c100a91f0af0782387a2440ee7fcc79aabe78a11a7e6816d7991ff5e74b8227c9040a155c62a8'
